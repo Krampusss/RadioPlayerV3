683a21a1142926172f2dfa9145044e507a25d7b0
Krampusss <90748072+Krampusss@users.noreply.github.com>
2021-10-23 10:30:46 +0530

    Update player.py

--- plugins/bot/player.py
+++ plugins/bot/player.py
@@ -566,12 +566,12 @@ allcmd = ["play", "current", "playlist", "song", f"song@{USERNAME}", f"play@{USE
 async def not_chat(_, m: Message):
     buttons = [
             [
-                InlineKeyboardButton("CHANNEL", url="https://t.me/AsmSafone"),
-                InlineKeyboardButton("SUPPORT", url="https://t.me/AsmSupport"),
+                InlineKeyboardButton("CHANNEL", url="https://t.me/International_chatting_Legends"),
+                InlineKeyboardButton("SUPPORT", url="https://t.me/truewildthoughts"),
             ],
             [
-                InlineKeyboardButton("🤖 MAKE YOUR OWN BOT 🤖", url="https://heroku.com/deploy?template=https://github.com/AsmSafone/RadioPlayerV3"),
+                InlineKeyboardButton("🤖 MAKE YOUR OWN BOT 🤖", url="https://t.me/International_chatting_Legends"),
             ]
          ]
-    k=await m.reply_photo(photo="https://telegra.ph/file/4e839766d45935998e9c6.jpg", caption="**Sorry, You Can't Use This Bot In This Group! 🤷‍♂️ But You Can Make Your Own Bot Like This From The [Source Code](https://github.com/AsmSafone/RadioPlayerV3) Below 😉!**", reply_markup=InlineKeyboardMarkup(buttons))
+    k=await m.reply_photo(photo="https://telegra.ph/file/b29ef5f1d73cf41f61bf0.jpg", caption="**Sorry, You Can't Use This Bot In This Group! 🤷‍♂️ But You Can Make Your Own Bot Like This From The [Source Code](https://t.me/International_chatting_Legends) Below 😉!**", reply_markup=InlineKeyboardMarkup(buttons))
     await mp.delete(m)
