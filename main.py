5842289ab64cf311d7c2f16c7539d02aad7df94a
Krampusss <90748072+Krampusss@users.noreply.github.com>
2021-10-23 17:15:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ async def main():
     async with bot:
         await mp.start_radio()
         try:
-            await USER.join_chat("AsmSafone")
+            await USER.join_chat("International_chatting_Legends")
         except UserAlreadyParticipant:
             pass
         except Exception as e:
@@ -64,7 +64,7 @@ def stop_and_restart():
 
 bot.run(main())
 bot.start()
-print("\n\nRadio Player Bot Started, Join @AsmSafone!")
+print("\n\nRadio Player Bot Started, Join @International_chatting_Legends!")
 bot.send(
     functions.bots.SetBotCommands(
         commands=[
@@ -179,11 +179,11 @@ async def restart(_, message: Message):
             target=stop_and_restart()
             ).start()
     try:
-        await k.edit("✅ **Restarted Successfully! \nJoin @AsmSafone For Update!**")
+        await k.edit("✅ **Restarted Successfully! \nJoin @International_chatting_Legends For Update!**")
         await k.reply_to_message.delete()
     except:
         pass
 
 idle()
-print("\n\nRadio Player Bot Stopped, Join @AsmSafone!")
+print("\n\nRadio Player Bot Stopped, Join @International_chatting_Legends!")
 bot.stop()
